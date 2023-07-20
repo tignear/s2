7d58f04809955af43c7ff926886991dd859c2c5a
tig <tignear+m@gmail.com>
2023-07-20 17:58:16 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,105 @@
-use std::f64::consts::PI;
 use std::collections::HashMap;
-const dt:f64=0.01;
-const c1:f64 = 0.0;
-const G:f64 = 79E9;
-const d_d:f64 = 0.15;
-const A:f64 = PI / 4.0 * d_d * d_d;
-const m:f64 = 6.0;
-const h_b:f64 = 0.05;
-const h:f64 = h_b;
-const l_a:f64 = 0.6;
-const l_b:f64= 0.93;
-const d_B:f64 = 0.5;
-const l:f64 = d_B / 2.0 + (l_a + l_b) - 0.05;
-const M_B :f64= 70.0;
-const d_a :f64= 0.1;
-const M_a :f64= 4.0;
-const I_Ga:f64 = (d_a * d_a / 16.0 + l_a * l_a / 12.0) * M_a;
-const I_GB:f64 = 1.0 / 8.0 * M_B * d_B * d_B;
-const M_b:f64 = 1.0;
-const I_Gb:f64 = 1.0 / 12.0 * M_b * (h_b * h_b + l_b * l_b);
-const I:f64 = I_GB
-        + (I_Ga + 1.0 / 4.0 * M_a * (d_B + l_a) * (d_B + l_a))
-        + I_Gb
-        + 1.0 / 4.0 * M_b * (d_B + 2.0 * l_a + l_b) * (d_B + 2.0 * l_a + l_b);
-const n:f64=10.0;
-struct ComputeEnv{
+use std::f64::consts::PI;
+const dt: f64 = 0.000001;
+const G: f64 = 79E9;
+const d_d: f64 = 0.15;
+const A: f64 = PI / 4.0 * d_d * d_d;
+const m: f64 = 6.0;
+const h_b: f64 = 0.05;
+const h: f64 = h_b;
+const l_a: f64 = 0.6;
+const l_b: f64 = 0.93;
+const d_B: f64 = 0.5;
+const l: f64 = d_B / 2.0 + (l_a + l_b) - 0.05;
+const M_B: f64 = 70.0;
+const d_a: f64 = 0.1;
+const M_a: f64 = 4.0;
+const I_Ga: f64 = (d_a * d_a / 16.0 + l_a * l_a / 12.0) * M_a;
+const I_GB: f64 = 1.0 / 8.0 * M_B * d_B * d_B;
+const M_b: f64 = 1.0;
+const I_Gb: f64 = 1.0 / 12.0 * M_b * (h_b * h_b + l_b * l_b);
+const I: f64 = I_GB
+    + (I_Ga + 1.0 / 4.0 * M_a * (d_B + l_a) * (d_B + l_a))
+    + I_Gb
+    + 1.0 / 4.0 * M_b * (d_B + 2.0 * l_a + l_b) * (d_B + 2.0 * l_a + l_b);
+
+struct ComputeEnv {
     F0: f64,
-    cache_x:  HashMap<i32,f64>,
-    cache_dxdt: HashMap<i32,f64>,
+    n: f64,
+    cache_x: HashMap<i32, f64>,
+    cache_dxdt: HashMap<i32, f64>,
 }
-impl ComputeEnv{
-    fn x(&mut self,step: i32)->f64{
-        let r = match self.cache_x.get(&step){
+impl ComputeEnv {
+    fn x(&mut self, step: i32) -> f64 {
+        let r = match self.cache_x.get(&step) {
             Some(x) => *x,
-            None=>{
-                let t = f64::from(step)*dt;
-                if t < -0.3{
-                    return 0.0
+            None => {
+                let t = f64::from(step) * dt;
+                if t < -0.3 {
+                    return 0.0;
                 }
-                let x = self.x(step-1)+self.dxdt(step-1)*dt+self.dxdt2(step-1)*dt*dt;
+                let x =
+                    self.x(step - 1) + self.dxdt(step - 1) * dt + self.dxdt2(step - 1) * dt * dt;
                 self.cache_x.insert(step, x);
+                // println!("x@{step}:{x}");
                 x
             }
         };
-        println!("x@{step}:{r}");
+
         r
     }
 
-    fn dxdt2(&mut self,step: i32) -> f64{
-        let t = f64::from(step)*dt;
-        self.F0/m*phi_n(t)-G*A/(h*m)*self.x(step)
+    fn dxdt2(&mut self, step: i32) -> f64 {
+        let t = f64::from(step) * dt;
+        self.F0 / m * self.phi_n(t) - G * A / (h * m) * self.x(step)
     }
 
-    fn dxdt3(&mut self,step: i32) ->f64{
-        let t = f64::from(step)*dt;
-        self.F0/m*dphin_ndt(t)-G*A/(h*m)*self.dxdt(step)
+    fn dxdt3(&mut self, step: i32) -> f64 {
+        let t = f64::from(step) * dt;
+        self.F0 / m * self.dphin_ndt(t) - G * A / (h * m) * self.dxdt(step)
     }
 
-    fn dxdt(&mut self,step: i32) -> f64 {
-        let t = f64::from(step)*dt;
-        let r= match self.cache_dxdt.get(&step) {
+    fn dxdt(&mut self, step: i32) -> f64 {
+        let t = f64::from(step) * dt;
+        let r = match self.cache_dxdt.get(&step) {
             Some(dxdt) => *dxdt,
             None => {
-                if t< -0.3 {
+                if t < -0.3 {
                     return 0.0;
                 }
-                
-                let dxdt = self.dxdt(step-1)+self.dxdt2(step-1)*dt+self.dxdt3(step-1)*dt*dt;
+
+                let dxdt = self.dxdt(step - 1)
+                    + self.dxdt2(step - 1) * dt
+                    + self.dxdt3(step - 1) * dt * dt;
                 self.cache_dxdt.insert(step, dxdt);
+                // println!("dx@{step}:{dxdt}");
                 dxdt
             }
         };
-        println!("dx@{step}:{r}");
+
         r
     }
+    fn phi_n(&self, t: f64) -> f64 {
+        f64::sqrt(self.n / PI) * f64::exp(-self.n * t * t)
+    }
+    fn dphin_ndt(&self, t: f64) -> f64 {
+        2.0 * t * (-self.n) * f64::sqrt(self.n / PI) * f64::exp(-self.n * t * t)
+    }
 }
-fn phi_n( t: f64) -> f64 {
-    f64::sqrt(n / PI) * f64::exp(-n * t * t)
-}
-fn dphin_ndt(t :f64) ->f64{
-    2.0*t*(-n)*f64::sqrt(n / PI)*f64::exp(-n * t * t)
-}
-
-fn main() {
-
-    let omega = PI * 33.0; //2.0 * PI * 33.0;
+fn compute_f0(omega: f64) -> (f64, f64) {
     let mut F0_min = 0.0;
-    let mut F0_max = 1E10;
-
-    for _ in 0..1 {
-        let F0 = F0_max / 2.0 + F0_min / 2.0;
-        let mut env = ComputeEnv{
+    let mut F0_max = 1000000.0;
+    let mut F0 = 0.0;
+    let mut d = 0.0;
+    for _ in 0..100 {
+        F0 = F0_max / 2.0 + F0_min / 2.0;
+        let mut env = ComputeEnv {
             F0,
+            n: 1000.0,
             cache_dxdt: HashMap::new(),
-            cache_x: HashMap::new()
+            cache_x: HashMap::new(),
         };
-        let left = env.dxdt(60);
+        let left = env.dxdt(300000);
 
         let right = l * (omega - l * F0 / I);
         if left > right {
@@ -103,6 +107,30 @@ fn main() {
         } else {
             F0_min = F0;
         }
-        println!("{},{},{}", F0,left, left - right);
+        d = (left - right).abs();
+        if d < 0.01 {
+            break;
+        }
     }
+    return (F0, d);
+}
+
+fn sub_main() {
+    //2.0 * PI * 33.0;
+
+    for s in 0..66 {
+        let omega = PI * f64::from(s);
+        let (F0, d) = compute_f0(omega);
+
+        println!("{},{},{}", omega, F0, d);
+    }
+}
+fn main() {
+    const STACK_SIZE: usize = 512 * 1024 * 1024;
+    std::thread::Builder::new()
+        .stack_size(STACK_SIZE)
+        .spawn(sub_main)
+        .unwrap()
+        .join()
+        .unwrap();
 }
